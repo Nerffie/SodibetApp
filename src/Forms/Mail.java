7b539a4ec4ee6be370d98ea320b63c2ef01ab3c4
Nerffie <nerffye@gmail.com>
2019-07-27 14:48:50 +0200

    attempt fix for mail sending

--- src/Forms/Mail.java
+++ src/Forms/Mail.java
@@ -59,6 +59,7 @@ public class Mail {
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
 		
